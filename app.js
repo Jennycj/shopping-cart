3b54ee72572b0b52a2cdb78d9797ea82a10c58c8
Jennycj <ezeobijennifer2@gmail.com>
2022-01-16 05:46:02 +0100

    refactor and clean up code

--- app.js
+++ app.js
@@ -27,37 +27,27 @@ pool.getConnection((err, connection) => {
     console.log('db connected'); 
     pool.query("CREATE DATABASE IF NOT EXISTS shop", function (err, result) {
       if (err) throw err;
-      console.log("Database created");
     }); 
 
     const userSql = "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY auto_increment, username VARCHAR(25) UNIQUE NOT NULL, password CHAR(60) NOT NULL, email VARCHAR(100) UNIQUE NOT NULL)";
     pool.query(userSql, function (err, result) {
       if (err) throw err;
-      console.log("Table created");
     });
 
     const productSql = "CREATE TABLE IF NOT EXISTS products (id INT PRIMARY KEY auto_increment, title VARCHAR(25) NOT NULL, description CHAR(60) NOT NULL, sku VARCHAR(10) UNIQUE NOT NULL, qty INT NOT NULL, category VARCHAR(25) NOT NULL, price INT NOT NULL, expiry DATE)";
     pool.query(productSql, function (err, result) {
       if (err) throw err;
-      console.log("product table created");
     });
 
     const cartSql = "CREATE TABLE IF NOT EXISTS carts (id INT PRIMARY KEY auto_increment, user_id INT)";
     pool.query(cartSql, function (err, result) {
       if (err) throw err;
-      console.log("cart table created");
     });
 
     const pivotSql = "CREATE TABLE IF NOT EXISTS cartProducts (id INT PRIMARY KEY auto_increment, product_id INT, cart_id INT, qty INT)";
     pool.query(pivotSql, function (err, result) {
       if (err) throw err;
-      console.log("cartProducts table created");
     });
-})
-app.use('/health', (req, res) => {
-  return res.json({ 
-    message: "Working"
-  });
 })
 
 app.use('/', productRouter)
